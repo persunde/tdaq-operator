4935e92f41e4784d4a4722c9f1a4bd3e22a30da3
Per Sunde <sunde.per92@gmail.com>
2020-08-26 12:42:41 +0200

    added unfinished deleteIfFinished functions and lots of todos

--- src/main/java/ch/cern/tdaq/k8s/operator/RunController.java
+++ src/main/java/ch/cern/tdaq/k8s/operator/RunController.java
@@ -1,29 +1,27 @@
 package ch.cern.tdaq.k8s.operator;
 
 import ch.cern.tdaq.k8s.operator.CustomResource.RunResource;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.containersolutions.operator.api.Context;
 import com.github.containersolutions.operator.api.Controller;
 import com.github.containersolutions.operator.api.ResourceController;
 import com.github.containersolutions.operator.api.UpdateControl;
-import io.fabric8.kubernetes.api.model.apiextensions.CustomResourceDefinition;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.apps.DeploymentList;
+import io.fabric8.kubernetes.api.model.apps.DeploymentStatus;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller(customResourceClass = RunResource.class,
         crdName = "runresource.operator.tdaq.cern.ch")
 public class RunController implements ResourceController<RunResource> {
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final KubernetesClient kubernetesClient;
+    private final int runNumberPaddingSize = 100000;
 
     public RunController(KubernetesClient kubernetesClient) {
         this.kubernetesClient = kubernetesClient;
@@ -42,7 +40,7 @@ public class RunController implements ResourceController<RunResource> {
         String runPipe = resource.getSpec().getRunPipe();
 
         try {
-            createOrReplaceDeployment(runNumber, runPipe);
+            createNewDeployment(runNumber, runPipe);
         } catch (IOException e) {
             e.printStackTrace();
             log.error("createOrReplaceDeployment failed", e);
@@ -59,9 +57,10 @@ public class RunController implements ResourceController<RunResource> {
      * @throws IOException
      */
     @NotNull
-    private void createOrReplaceDeployment(int runNumber, String runPipe) throws IOException {
+    private void createNewDeployment(int runNumber, String runPipe) throws IOException {
         /**
          * TODO: insert the runNumber into the deployment, because the runNumber needs to be accessible as an enviroment variable for all containers in the Pod
+         * TODO: make sure you add the runNumberPaddingSize to the deployment name!
          */
         String deploymentYamlPath = "worker-deployment.yaml"; /* Should we let the users use their own yaml, or yaml that is part of the JAR under /resources ? */
         try (InputStream yamlInputStream = getClass().getResourceAsStream(deploymentYamlPath)) {
@@ -78,4 +77,49 @@ public class RunController implements ResourceController<RunResource> {
             throw ex;
         }
     }
+
+    /**
+     * Deletes deployment running Pods with an old RunNumber if all the pods are completed
+     * @throws IOException
+     */
+    @NotNull
+    private void deleteOldDeploymentIfFinished(int currentRunNumber) throws IOException {
+        DeploymentList deploymentList = kubernetesClient.apps().deployments().inNamespace("default").list();
+        for (Deployment deployment : deploymentList.getItems()) {
+            /**
+             * TODO: Test which of these replica count values are the one I want to use!?
+             */
+            Integer currentReplicas = deployment.getStatus().getReplicas(); /* This should be currently running, bad and good. Probably the one I want, test!! */
+            /*
+            deployment.getStatus().getAvailableReplicas();
+            deployment.getStatus().getReadyReplicas();
+            deployment.getStatus().getUnavailableReplicas();
+            deployment.getStatus().getUpdatedReplicas();
+            DeploymentStatus stat = deployment.getStatus();
+             */
+
+            /*
+            deployment.getSpec().getReplicas(); // This probably has the wanted number of replicas
+            */
+
+            String deploymentName = deployment.getMetadata().getName();
+            /**
+             * TODO: Unsure about how to handle null values. What does "null" mean? That the value is inaccessible, should we delete or not when null?
+             */
+            if (currentReplicas != null && currentReplicas == 0 && !isCurrentDeployment(deploymentName, currentRunNumber)) {
+                /**
+                 * TODO: delete the deployment
+                 */
+            }
+        }
+    }
+
+    private boolean isCurrentDeployment(String deploymentName, int currentRunNumber) {
+        /**
+         * TODO: for this to work, runNumberPaddingSize must be the same as used in the actual deployment name!
+         */
+        String formattedRunNumber = String.format("%0" + runNumberPaddingSize + "d", currentRunNumber);
+        return deploymentName.contains(formattedRunNumber);
+    }
+
 }
