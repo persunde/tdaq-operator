497123d059ccfc25c60a64df6735d02dd8bda37a
Per Sunde <sunde.per92@gmail.com>
2020-08-31 17:52:28 +0200

    created standalone webserver that the new clients can query if they should stop or not, also includes yaml files for both. Added CRD and and CR yaml files

---
+++ webclient/webclient.py
@@ -0,0 +1,22 @@
+# A test program used to test TDAQ Run Operator. Will run until the Webserver says there is a higher run_number than this Pod is "processing data" for
+import urllib.request
+import sys, os
+import json
+import time
+from random import randrange
+  
+#arguments_list = sys.argv[1:]
+#run_number = arguments_list[0]
+run_number = os.environ['RUN_NUMBER']
+WEBSERVER_SERVICE_SERVICE_HOST = os.environ['WEBSERVER_SERVICE_SERVICE_HOST']
+WEBSERVER_SERVICE_SERVICE_PORT = os.environ['WEBSERVER_SERVICE_SERVICE_PORT']
+url = "http://" + WEBSERVER_SERVICE_SERVICE_HOST + ":" + WEBSERVER_SERVICE_SERVICE_PORT + "/?run=" + run_number
+  
+while True:  
+    contents = urllib.request.urlopen(url).read()
+    json_content = json.loads(contents)
+    if json_content["shutdown"] == "true" or json_content["shutdown"] == "True":
+        exit(os.EX_OK)
+    # Sleep random time in range [4, 30), increments steps by 2
+    sleep_time = randrange(4, 30, 2)
+    time.sleep(sleep_time)
---
+++ webserver/webserver.js
@@ -0,0 +1,30 @@
+/**
+ * This program is used to tell the K8S Pods to shutdown or not, based on their runNUmber that they send in as a GET param:
+ * 
+ * How to send a GET request:
+ *  GET HOST:PORT/?run=<runNumber>
+ */
+
+var http = require('http');
+let latestRunNumber = 0;
+let totalCalls = 0;
+
+const server = http.createServer();
+server.on('request', async (req, res) => {
+    const newRunNumber = parseInt(req.query.run, 10);
+    let shutdown = false;
+    if (latestRunNumber > newRunNumber) {
+        shutdown = true;
+    } else if (latestRunNumber < newRunNumber) {
+        latestRunNumber = newRunNumber;
+    }
+
+    totalCalls++;
+
+    const response = JSON.stringify({"shutdown": shutdown, "totalCalls": totalCalls});
+    res.writeHead(200);
+    res.setHeader('Content-Type', 'application/json');
+    res.end(response);
+});
+
+server.listen(8080);
